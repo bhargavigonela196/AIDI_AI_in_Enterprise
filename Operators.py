b8ffffd009dbc68dbc4d7633f5e5138c5caa8879
bhargavigonela196 <bhargavigonela196@gmail.com>
2025-02-05 17:49:38 -0500

    Adding test file

---
+++ Operators.py
@@ -0,0 +1,13 @@
+# Arthemetic Operators
+a=2
+b=3
+c=a+b
+print(c)
+d=a**b
+print(d)
+# Floor division returns the round figure of the quiofcient
+e=b//a
+print(e)
+# % retuns remainder
+
+# Assignment operators
